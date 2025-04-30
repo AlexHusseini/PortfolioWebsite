da8803d23b0e55b546cf066cabee5405aef76048
AlexHusseini <177381913+AlexHusseini@users.noreply.github.com>
2025-04-29 22:50:32 -0400

    added icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,24 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Alexander Husseini | Portfolio',
   description: 'My Work Portfolio',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
