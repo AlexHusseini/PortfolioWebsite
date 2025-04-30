920fd7d011bafe0046f8e092328913dfc2e0dbfe
AlexHusseini <177381913+AlexHusseini@users.noreply.github.com>
2025-04-29 23:31:09 -0400

    added more detail

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,6 +32,15 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta property="og:title" content="Alexander Husseini | Portfolio" />
+        <meta property="og:description" content="My Work Portfolio" />
+        <meta property="og:image" content="/android-chrome-512x512.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://portfolio-d040a.web.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Alexander Husseini | Portfolio" />
+        <meta name="twitter:description" content="My Work Portfolio" />
+        <meta name="twitter:image" content="/android-chrome-512x512.png" />
       </head>
       <body className={inter.className}>
         <ThemeProvider>
